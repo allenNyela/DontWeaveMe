8d61b2c3f0a8a16742d07361ce338fea62a640df
Nyela Allen <58515942+allenNyela@users.noreply.github.com>
2026-01-11 16:43:40 -0800

    updated webnode mechanics

--- Weave/Assets/Scripts/GamePlay/CustomWebNode.cs
+++ Weave/Assets/Scripts/GamePlay/CustomWebNode.cs
@@ -3,4 +3,31 @@ using UnityEngine;
 public class CustomWebNode : WebNode
 {
     public Vector2 customPosition;
+    public bool onPlatform = false;
+
+    void OnTriggerEnter2D(Collider2D interactable)
+    {
+        if (interactable.CompareTag("Platform"))
+        {
+            onPlatform = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D interactable)
+    {
+        if (interactable.CompareTag("Platform"))
+        {
+            onPlatform = false;
+        }
+    }
+
+    void OnTriggerStay2D(Collider2D interactable)
+    {
+        if (!onPlatform && interactable.CompareTag("Platform"))
+        {
+            onPlatform = true;
+        }
+    }
 }
+
+
