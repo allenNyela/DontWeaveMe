1593dfa1530000588d855001a4c5347dc32d1eef
Harry <harryli0915@gmail.com>
2026-01-16 19:20:07 -0800

    node platform fix range bug

--- Weave/Assets/Scripts/GamePlay/SpiderController.cs
+++ Weave/Assets/Scripts/GamePlay/SpiderController.cs
@@ -327,11 +327,14 @@ public class SpiderController : MonoBehaviour
 
     private void DropNode()
     {
+        if (!nearPlatform || nodePlatform == null)
+            return;
         Debug.Log("Dropping Node");
         GameObject newNode = Instantiate(WeaveBoardManager.instance.nodeObject, transform.position, Quaternion.identity, GameManager.Instance.BoardRoot.gameObject.transform);
         var customNode = newNode.GetComponent<CustomWebNode>();
         customNode.gridPos = WeaveBoardManager.instance.WorldToGridPos(transform.position);
         numberOfNodesAvailable--;
+        newNode.transform.parent = nodePlatform.transform;
     }
 
 
